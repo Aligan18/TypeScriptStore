6d2921c972106195ea28902272b098b0f50ed002
Aligan18 <alisherganiev18@gmail.com>
2023-07-12 16:37:00 +0600

    I changed the StarIcon

--- src/components/StarGroup/StarIcon/StarIcon.tsx
+++ src/components/StarGroup/StarIcon/StarIcon.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import { IStarIconProps } from './StarIcon.props'
-import cn from 'classnames'
-import classes from './StarIcon.module.scss'
+import React from "react";
+import { IStarIconProps } from "./StarIcon.props";
 
-const StarIcon = ({filled, ...props  }:IStarIconProps) => {
-  return (<div {...props}> 
-    {filled?<i className={cn("fa-solid fa-star", classes.star)}></i>
-    :
+import classes from "./StarIcon.module.scss";
+
+const StarIcon = ({ filled, ...props }: IStarIconProps) => {
+  return (
+    <div {...props}>
+      {filled ? (
+        <i className={"fa-solid fa-star" + classes.star}></i>
+      ) : (
         <i className="fa-regular fa-star"></i>
-  }
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default StarIcon
+export default StarIcon;
